fab0b3ed8528cfc02a8f5c9ba5cd5a6361e2f326
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-01-04 18:21:02 +0330

    added menu

--- src/index.js
+++ src/index.js
@@ -1,50 +1,38 @@
 let menu = ["Roja", "About", "Work", "Les Chats!", "Contact"];
 let menuBar = document.getElementById("nav").children;
-function regularMenu() {
+function setMenu() {
   for (i = 0; i < menu.length; i++) {
     menuBar[i].innerHTML = menu[i];
   }
 }
-function swapMenu(n) {
-  menu.push(menu[0]);
-  menu.shift();
-  for (i = 0; i < menu.length; i++) {
-    menuBar[i].innerHTML = menu[i];
-  }
-}
+setMenu();
 let container = document.getElementById("container");
-let firstItem = document.getElementById("menu-first-item");
-let int = 0;
+let firstItem = document.getElementById("roja");
+let navbar = document.getElementById("nav");
 function changeMenu() {
   console.log(container.scrollTop);
   if (container.scrollTop <= 500 && container.scrollTop >= 0) {
     menu = ["Roja", "About", "Work", "Les Chats!", "Contact"];
-    regularMenu();
-    function move() {
-      if (int === 700) {
-        clearInterval(t);
-      }
-      int += 42;
-      firstItem.style.transform = "translateX(" + int + "px)";
-      console.log(int);
+    setMenu();
+    firstItem.classList.remove("hidden");
+    firstItem.style.order = "-1";
+  }
+  if (container.scrollTop <= 626 && container.scrollTop >= 500) {
+    firstItem.classList.add("hidden");
+    function test() {
+      console.log("Hi!");
+      firstItem.style.order = "1";
     }
-    let t = setInterval(move, 1000);
+    firstItem.addEventListener("transitionend", test);
   }
 
-  if (container.scrollTop >= 626) {
-    menu = ["About", "Work", "Les Chats!", "Contact", "Roja"];
-    for (i = 0; i < menu.length; i++) {
-      menuBar[i].innerHTML = menu[i];
-    }
-  }
+  // if (container.scrollTop >= 626) {
+  //   menu = ["About", "Work", "Les Chats!", "Contact", "Roja"];
+  //   firstItem.classList.add("visibl");
+  //   firstItem.classList.remove("hidden");
+  //   for (i = 0; i < menu.length; i++) {
+  //     menuBar[i].innerHTML = menu[i];
+  //   }
+  // }
 }
 container.addEventListener("scroll", changeMenu);
-console.log(document.getElementById("roja-page"));
-console.log(document.getElementById("about-page"));
-// if (
-//   document.getElementById("roja-page").nextElementSibling == "div#about-age"
-// ) {
-//   for (i = 0; i < menu.length; i++) {
-//     menuBar[i].innerHTML = "About";
-//   }
-// }
