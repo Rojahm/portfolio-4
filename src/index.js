3783fde13c8e5afe69dbd5997812286489a2555b
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-01-06 02:52:59 +0330

    added hero page about page-menu-footer-structure

--- src/index.js
+++ src/index.js
@@ -1,38 +1,5 @@
-let menu = ["Roja", "About", "Work", "Les Chats!", "Contact"];
-let menuBar = document.getElementById("nav").children;
-function setMenu() {
-  for (i = 0; i < menu.length; i++) {
-    menuBar[i].innerHTML = menu[i];
-  }
-}
-setMenu();
-let container = document.getElementById("container");
-let firstItem = document.getElementById("roja");
-let navbar = document.getElementById("nav");
+let container = document.getElementById("scale");
 function changeMenu() {
   console.log(container.scrollTop);
-  if (container.scrollTop <= 500 && container.scrollTop >= 0) {
-    menu = ["Roja", "About", "Work", "Les Chats!", "Contact"];
-    setMenu();
-    firstItem.classList.remove("hidden");
-    firstItem.style.order = "-1";
-  }
-  if (container.scrollTop <= 626 && container.scrollTop >= 500) {
-    firstItem.classList.add("hidden");
-    function test() {
-      console.log("Hi!");
-      firstItem.style.order = "1";
-    }
-    firstItem.addEventListener("transitionend", test);
-  }
-
-  // if (container.scrollTop >= 626) {
-  //   menu = ["About", "Work", "Les Chats!", "Contact", "Roja"];
-  //   firstItem.classList.add("visibl");
-  //   firstItem.classList.remove("hidden");
-  //   for (i = 0; i < menu.length; i++) {
-  //     menuBar[i].innerHTML = menu[i];
-  //   }
-  // }
 }
 container.addEventListener("scroll", changeMenu);
